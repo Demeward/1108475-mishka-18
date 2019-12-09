d38e130764f3e30765920f406220eae5e9ee4a03
Demeward <did.dod1520@mail.ru>
2019-12-09 11:08:10 +0300

    Меню и модальное окно

---
+++ source/js/app.js
@@ -0,0 +1,47 @@
+var menuButton = document.querySelector(".main-nav__toggle");
+var menu = document.querySelector(".main-nav");
+
+menu.classList.remove("main-nav--no-js");
+menuButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  menu.classList.toggle("main-nav--closed");
+});
+
+var promoButton = document.querySelector(".promo__order");
+var overlay = document.querySelector(".modal-overlay");
+var modal = document.querySelector(".modal");
+var catalogButton = document.querySelectorAll(".catalog__basket");
+
+if (promoButton) {
+  promoButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.add("modal--show");
+    overlay.classList.add("modal-overlay--show");
+  })
+};
+
+if (catalogButton) {
+  for (var i = 0; i < catalogButton.length; i++) {
+    catalogButton[i].addEventListener('click', function(evt) {
+      evt.preventDefault();
+      modal.classList.add('modal--show');
+      overlay.classList.add('modal-overlay--show');
+    });
+  }
+}
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault(); {
+      modal.classList.remove("modal--show");
+      overlay.classList.remove("modal-overlay--show");
+    }
+  }
+});
+
+
+overlay.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal--show");
+  overlay.classList.remove("modal-overlay--show");
+});
